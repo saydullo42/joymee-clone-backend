9a2b0afa90e711bc33b9fcdb0415ef56272630a6
saydullo42 <42saydullo@gmail.com>
2026-04-01 12:08:06 +0500

    base model updated

--- base/models.py
+++ base/models.py
@@ -5,7 +5,8 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
+    class Meta:
+        abstract = True
 
 
 class Country(BaseModel):
